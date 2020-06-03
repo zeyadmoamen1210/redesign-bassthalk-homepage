7fc0e63d8b4ee66895cf7dd717de2c5b8525bf0c
zeyadMoamen <zeyad.moamen@hamza.solutions>
2020-06-03 11:33:56 +0200

    lesson explain\summary page

--- pages/lesson.vue
+++ pages/lesson.vue
@@ -9,7 +9,9 @@
               <div class="lesson-inner-grid">
                 <div class="row">
                   <div class="col-md-4">
-                    <h6 class="subject-name">الكمياء</h6>
+                    <h6 class="subject-name">
+                      <img src="../assets/imgs/laboratory-1.png" alt="">
+                      الكمياء</h6>
                   </div>
                   <div class="col-md-8">
                     <div class="unit lesson-unit">
@@ -25,13 +27,17 @@
                 <div class="row">
                   <div class="col-md-6">
                     <NuxtLink :to="'/lesson/explain'" active-class="active">
-                      <button class="explain-btn">الشرح</button>
+                      <button class="explain-btn">
+                        <img style="margin-left: 9px;" src="../assets/imgs/noun_teaching_-1.png" alt="">
+                        الشرح</button>
                     </NuxtLink>
                   </div>
                    <div class="col-md-6">
 
                     <NuxtLink :to="'/lesson/summary'">
-                      <button class="explain-btn">الملخص</button>
+                      <button class="explain-btn">
+                        <img style="margin-left: 9px;" src="../assets/imgs/noun_PDF_-1.png" alt="">
+                        الملخص</button>
                     </NuxtLink>
 
                   </div>
@@ -82,9 +88,16 @@ components:{
     }
   }
   h6.subject-name{
-          padding: 9px 0px;
-    font-size: 23px;
+         padding: 7px 0px;
+    font-size: 26px;
     color: #058ac6;
+    img{
+          display: block;
+    width: 23%;
+    float: right;
+    margin-left: 10px;
+    padding-top: 0;
+    }
   }
 
   }
@@ -99,6 +112,7 @@ components:{
     padding: 13px;
     font-family: "CustomFontBold";
     color: #FFF;
+    margin-bottom: 18px;
     }
     .explain-btn{
       background: #DDD;
@@ -106,4 +120,47 @@ components:{
 
     }
   }
+
+  // Extra small devices (portrait phones, less than 576px)
+@media (max-width: 575.98px) {
+.lesson h6.subject-name img{
+    width: 12%;
+    margin-bottom: 30px;
+}
+.lesson .lesson-unit {
+    margin-bottom: 23px;
+}
+
+.video-bannar img{
+      left: 67px;
+}
+.video-bannar span{
+        left: 67px;
+
+}
+ }
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: 576px) and (max-width: 767.98px) {
+
+ }
+
+// Medium devices (tablets, 768px and up)
+@media (min-width: 768px) and (max-width: 991.98px) {
+.lesson-btns {
+     padding: 0;
+}
+.lesson h6.subject-name img{
+      width: 30%;
+}
+ }
+
+// Large devices (desktops, 992px and up)
+@media (min-width: 992px) and (max-width: 1199.98px) {
+
+ }
+
+// Extra large devices (large desktops, 1200px and up)
+@media (min-width: 1200px) {
+}
 </style>
