da78cf5d76530bb553178893806b6ad2d7a129c0
zeyadMoamen <zeyad.moamen@hamza.solutions>
2020-06-03 04:21:06 +0200

    Best student page

--- pages/best-students.vue
+++ pages/best-students.vue
@@ -399,4 +399,45 @@ components:{
       }
     }
 
+
+
+
+// Extra small devices (portrait phones, less than 576px)
+@media (max-width: 575.98px) {
+.all-the-best-student .stu-info .stu-pic{
+margin: 6px 8px;
+}
+
+.all-the-best-student .stu-info .stu-num{
+      padding: 4px 9px;
+}
+
+.all-the-best-student .stu-info .stu-pic{
+  width: 48px;
+}
+.all-the-best-student .stu-info .stu-name{
+      font-size: 15px;
+}
+ }
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: 576px) and (max-width: 767.98px) {
+
+ }
+
+// Medium devices (tablets, 768px and up)
+@media (min-width: 768px) and (max-width: 991.98px) {
+
+ }
+
+// Large devices (desktops, 992px and up)
+@media (min-width: 992px) and (max-width: 1199.98px) {
+
+ }
+
+// Extra large devices (large desktops, 1200px and up)
+@media (min-width: 1200px) {
+
+ }
+
 </style>
