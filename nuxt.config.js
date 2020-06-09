2be0af48255ef4f202707c8594d149a5d25b5752
Zeyad Moamen <zeyad.moamen@hamza.solutions>
2020-06-09 23:50:31 +0200

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,14 +64,7 @@ export default {
      */
     build: {
         extend(config, ctx) {
-          config.module.rules.push({
-            enforce: "pre",
-            test: /\.(js|vue)$/,
-            loader: "eslint-loader",
-            exclude: /(node_modules)/,
-            options: {
-              fix: true
-            }
+          
           })
         }
       }
