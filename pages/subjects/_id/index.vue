76a27d3ec89db0d1ad7daa25a35582f6ea4733ab
Zeyad Moamen <zeyad.moamen@hamza.solutions>
2020-07-20 15:28:56 +0200

    Add Subject student page

---
+++ pages/subjects/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="lecture-title">
+      <div class="row">
+         <div class="col-md-6">
+            <div class="lectures-title">
+              <nuxt-link :to="'/'+$route.params.id+'lectures'">المحاضرات</nuxt-link>
+            </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
