06f47c87e6f6f9fca9361bc17e28941e4e7c10ae
Zeyad Moamen <zeyad.moamen@hamza.solutions>
2020-07-21 20:07:48 +0200

    Convert questions into components

--- pages/general-exam-test.vue
+++ pages/general-exam-test.vue
@@ -36,10 +36,9 @@
         </div>
 
 
-        <form action="" >
           <div class="general-exam-content">
           <div class="row">
-            <div class="col-md-6" style="min-height: 320px;">
+            <div class="col-md-12" style="min-height: 320px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>1</h6>
@@ -47,30 +46,14 @@
 
                 <div><h6>السؤال الأول</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>صيغة السؤال هي عبارة عن صح او خطأ ؟</h6>
-
-                  <div style="overflow:hidden;display: block;float: left;width: 50%;">
-                  <input type="radio" name="check-ques-1" class="absthalk-radio" id="">
-                                <span>خطأ</span>
-                </div>
-
-
-               <div style="overflow:hidden;display: block;float: left;width: 50%;">
-                  <input type="radio" name="check-ques-1" class="absthalk-radio" id="">
-                                <span>صح</span>
-               </div>
-
-
-
-              </div>
+             <truefalse />
 
             </div>
 
 
 
 
-             <div class="col-md-6" style="min-height: 320px;">
+             <div class="col-md-12" style="min-height: 320px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>2</h6>
@@ -78,30 +61,12 @@
 
                 <div><h6>السؤال الثاني</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>صيغة السؤال هي عبارة عن صح او خطأ ؟</h6>
-
-                <img src="../assets/imgs/chemical-equation-color.jpg" alt="">
-
-                  <div style="overflow:hidden;display: block;float: left;width: 50%;">
-                  <input type="radio" name="check-ques-2" class="absthalk-radio" id="">
-                                <span>خطأ</span>
-                </div>
-
-
-               <div style="overflow:hidden;display: block;float: left;width: 50%;">
-                  <input type="radio" name="check-ques-2" class="absthalk-radio" id="">
-                                <span>صح</span>
-               </div>
-
-
-
-              </div>
+              <truefalse />
 
             </div>
 
 
-              <div class="col-md-6" style="margin-top: 25px;">
+              <div class="col-md-12" style="margin-top: 25px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>3</h6>
@@ -109,33 +74,12 @@
 
                 <div><h6>السؤال الثالث</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>صيغة السؤال هي عبارة عن صح او خطأ ؟</h6>
-
-                  <div class="ques-answer-btns">
-                    <div class="row">
-                      <div class="col-md-4">
-                        <button>اجابة 1</button>
-                      </div>
-
-                       <div class="col-md-4">
-                        <button>اجابة 2</button>
-                      </div>
-
-                       <div class="col-md-4">
-                        <button>اجابة 3</button>
-                      </div>
-                    </div>
-                  </div>
-
-
-
-              </div>
+              <choose />
 
             </div>
 
 
-             <div class="col-md-6" style="margin-top: 25px;">
+             <div class="col-md-12" style="margin-top: 25px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>4</h6>
@@ -143,33 +87,12 @@
 
                 <div><h6>السؤال الرابع</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>صيغة السؤال هي عبارة عن صح او خطأ ؟</h6>
-
-                  <div class="ques-answer-btns">
-                    <div class="row">
-                      <div class="col-md-4">
-                        <button>اجابة 1</button>
-                      </div>
-
-                       <div class="col-md-4">
-                        <button>اجابة 2</button>
-                      </div>
-
-                       <div class="col-md-4">
-                        <button>اجابة 3</button>
-                      </div>
-                    </div>
-                  </div>
-
-
-
-              </div>
+              <choose />
 
             </div>
 
 
-             <div class="col-md-6" style="margin-top: 25px;">
+             <div class="col-md-12" style="margin-top: 25px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>5</h6>
@@ -177,30 +100,14 @@
 
                 <div><h6>السؤال الخامس</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>أكمل الجملة التالية</h6>
-
-
-              <span class="fill-the-ques" style="width:100%;">
-
-              صيغة السؤال هي أكمل
-                <div class="form-groub" style="display: inline-block;">
-                  <input style="width:100%;padding: 6px" type="text" placeholder="ادخل أجابتك هنا">
-                </div>
-                الجملة
-               </span>
-
-
-
-
-              </div>
+              <complete />
 
             </div>
 
 
 
 
-            <div class="col-md-6" style="margin-top: 25px;">
+            <div class="col-md-12" style="margin-top: 25px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>6</h6>
@@ -208,28 +115,12 @@
 
                 <div><h6>السؤال السادس</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>أكمل الجملة التالية </h6>
-
-
-              <span class="fill-the-ques" style="width:100%;">
-
-              صيغة السؤال هي أكمل
-                <div class="form-groub" style="display: inline-block;">
-                  <input style="width:100%;padding: 6px" type="text" placeholder="ادخل أجابتك هنا">
-                </div>
-                الجملة
-               </span>
-
-
-
-
-              </div>
+              <complete />
 
             </div>
 
 
-             <div class="col-md-6" style="margin-top: 25px;">
+             <div class="col-md-12" style="margin-top: 25px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>7</h6>
@@ -237,28 +128,12 @@
 
                 <div><h6>السؤال السابع</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>صيغة السؤال هي عبارة عن صح او خطأ ؟</h6>
-
-                <img src="../assets/imgs/chemical-equation-color.jpg" alt="">
-
-
-               <div class="form-groub">
-                 <input style="width:100%;padding: 18px;" type="text" placeholder="ادخل أجابتك هنا">
-               </div>
-
-               <div class="file-choose">
-                 <input type="file" placeholder="قم بأرفاق صورة بالحل">
-                 <span> <img src="../assets/imgs/noun_Camera_1903011.png">  قم بأرفاق صورة بالحل </span>
-               </div>
-
-
-              </div>
+              <group />
 
             </div>
 
 
-             <div class="col-md-6" style="margin-top: 25px;">
+             <div class="col-md-12" style="margin-top: 25px;">
               <div class="exam-cont-item">
                     <div>
                   <h6>8</h6>
@@ -266,100 +141,36 @@
 
                 <div><h6>السؤال الثامن</h6></div>
               </div>
-              <div class="check-box-ques">
-                <h6>صيغة السؤال هي عبارة عن صح او خطأ ؟</h6>
-
-                <img src="../assets/imgs/chemical-equation-color.jpg" alt="">
-
-
-               <div class="form-groub">
-                 <input style="width:100%;padding: 18px;" type="text" placeholder="ادخل أجابتك هنا">
-               </div>
-
-               <div class="file-choose">
-                 <input type="file" placeholder="قم بأرفاق صورة بالحل">
-                 <span> <img src="../assets/imgs/noun_Camera_1903011.png">  قم بأرفاق صورة بالحل </span>
-               </div>
-
-
-              </div>
+              <group />
 
             </div>
 
 
-            <div class="col-md-6" style="margin-top: 25px;">
-              <div class="exam-cont-item">
-                    <div>
-                  <h6>9</h6>
-                </div>
+         
 
-                <div><h6>السؤال التاسع</h6></div>
-              </div>
-              <div class="check-box-ques">
-                <h6>قم بتسجيل صوتي بالأجابة</h6>
-
-
-              <div class="audio-div">
-                <div class="audio-icon">
-                  <img src="../assets/imgs/noun_record audio_1970418.png" alt="">
-                </div>
-                <audio id="player" controls src="">
-
-                </audio>
-
-
-              </div>
-
-
-
-
-              </div>
-
-            </div>
-
-             <div class="col-md-6" style="margin-top: 25px;">
-              <div class="exam-cont-item">
-                    <div>
-                  <h6>10</h6>
-                </div>
-
-                <div><h6>السؤال العاشر</h6></div>
-              </div>
-              <div class="check-box-ques">
-                <h6>قم بتسجيل صوتي بالأجابة</h6>
-
-
-              <div class="audio-div">
-                <div class="audio-icon">
-                  <img src="../assets/imgs/noun_record audio_1970418.png" alt="">
-                </div>
-                <audio id="player" controls src="">
-
-                </audio>
-
-
-              </div>
-
-
-
-
-              </div>
-
-            </div>
+           
 
             <input type="submit" value="تصحيح">
 
           </div>
         </div>
-        </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import truefalse from '../components/questions-type/truefalse'
+import group from '../components/questions-type/group'
+import choose from '../components/questions-type/choose'
+import complete from '../components/questions-type/complete'
 export default {
-
+  components:{
+    truefalse,
+    group,
+    choose,
+    complete
+  },
 methods:{
   playAudio: function(){
     document.getElementById('player').play()
