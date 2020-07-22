764b08e406100e62dd3347ccd5fc3a2585cd6859
Zeyad Moamen <zeyad.moamen@hamza.solutions>
2020-07-22 21:48:15 +0200

    Add tabs in subjects choices

--- pages/subjects/index.vue
+++ pages/subjects/index.vue
@@ -1,7 +1,63 @@
 <template>
   <div>
     <div class="container">
-      <div class="subjects">
+
+       <div class="subjects-navbar" style="text-align:center">
+         <button
+          :style="{
+            outline: 'none',
+            color: tabIndex == 1 ? '#46a8d5' : '',
+            'border-color': tabIndex == 1 ? '#46a8d5' : '',
+          }"
+          @click="(tabIndex = 1)"
+          type="line"
+        
+        >
+        <img src="../../assets/imgs/noun_knowledge_2042727.png" alt="">
+        الرئيسية</button>
+
+         <button
+          :style="{
+            outline: 'none',
+            color: tabIndex == 2 ? '#46a8d5' : '',
+            'border-color': tabIndex == 2 ? '#46a8d5' : '',
+          }"
+          @click="(tabIndex = 2)"
+          type="line"
+        
+        >
+        <img v-if="tabIndex !== 2" src="../../assets/imgs/noun_knowledge_2042727.png" alt="">
+        <img v-else src="../../assets/imgs/noun_knowledge_-1.png" alt="">
+        بنك المعلومات</button>
+        <button
+          :style="{
+            'padding-top': '5px',
+            outline: 'none',
+            color: tabIndex == 3 ? '#46a8d5' : '',
+            'border-color': tabIndex == 3 ? '#46a8d5' : '',
+          }"
+          @click="(tabIndex = 3)"
+          type="line"
+        >
+        <img v-if="tabIndex !== 3" src="../../assets/imgs/live-dark.png" alt="">
+        <img v-else src="../../assets/imgs/live-blue.png" alt="">
+        بث مباشر</button>
+
+         <button
+          :style="{
+            outline: 'none',
+            color: tabIndex == 4 ? '#46a8d5' : '',
+            'border-color': tabIndex == 4 ? '#46a8d5' : '',
+          }"
+          @click="(tabIndex = 4)"
+          type="line"
+        >
+        <img src="../../assets/imgs/noun_knowledge_2042727.png" alt="">
+        المتفوقين</button>
+       </div>
+
+
+      <div v-show="tabIndex == 1" class="subjects">
         <div class="form-title">
           <h4>
             <img src="../../assets/imgs/noun_classroom_-1.png" alt />
@@ -24,78 +80,121 @@
             </nuxt-link>
           </div>
 
-          <!-- 
-        <div class="col-md-3">
-            <nuxt-link to="">
-              <div class="subject-cont math">
-                <img src="../../assets/imgs/Blueprint-Cogwheel-Plan-Architecture-Construction-1.png" alt="">
-                <h3>الهندسة</h3>
-              </div>
-            </nuxt-link>
-        </div>
-
-          <div class="col-md-3">
-            <nuxt-link to="">
-              <div class="subject-cont arab">
-                <img src="../../assets/imgs/Page-3.png" alt="">
-                <h3>اللغة العريبة</h3>
-              </div>
-            </nuxt-link>
-        </div>
-
-          <div class="col-md-3">
-            <nuxt-link to="">
-              <div class="subject-cont math2">
-                <img src="../../assets/imgs/Page-4.png" alt="">
-                <h3>الجبر</h3>
-              </div>
-            </nuxt-link>
-        </div>
-
-          <div class="col-md-3">
-            <nuxt-link to="">
-              <div class="subject-cont eng">
-                <img src="../../assets/imgs/education-1.png" alt="">
-                <h3>اللغة الانجليزية</h3>
-              </div>
-            </nuxt-link>
-        </div>
-
-          <div class="col-md-3">
-            <nuxt-link to="">
-              <div class="subject-cont history">
-                <img src="../../assets/imgs/Outline-1.png" alt="">
-                <h3>التاريخ</h3>
-              </div>
-            </nuxt-link>
-        </div>
-
-          <div class="col-md-3">
-            <nuxt-link to="">
-              <div class="subject-cont physic">
-                <img src="../../assets/imgs/magnets-1.png" alt="">
-                <h3>الفزياء</h3>
-              </div>
-            </nuxt-link>
-        </div>
-
-          <div class="col-md-3">
-            <nuxt-link to="">
-              <div class="subject-cont geo">
-                <img src="../../assets/imgs/planet-earth-1.png" alt="">
-                <h3>الجغرافية</h3>
-              </div>
-            </nuxt-link>
-          </div>-->
-
-          <div class="annoncment" style="width: 100%; height: 200px;">
+          <!-- <div class="annoncment" style="width: 100%; height: 200px;">
             <img
               src="../../assets/imgs/Why-You-Should-Be-Focusing-Your-Marketing-Efforts-On-Mobile-Advertising-And-Social-Media.jpg"
               alt
             />
-          </div>
+          </div> -->
         </div>
       </div>
+
+
+
+
+
+      <div v-show="tabIndex == 2" class="subjects">
+        <div class="form-title">
+          <h4>
+            <img src="../../assets/imgs/noun_classroom_-1.png" alt />
+            المواد الدراسية
+          </h4>
+        </div>
+        <div class="row">
+          <div
+            class="col-md-3"
+            v-for="(subject, index) in subjects"
+            :key="index"
+          >
+            <nuxt-link :to="'/info-bank'">
+              <div class="subject-cont">
+                <div class="subject-icon-img">
+                  <img :src="subject.icon" alt />
+                </div>
+                <h3>{{ subject.nameAr }}</h3>
+              </div>
+            </nuxt-link>
+          </div>
+
+          <!-- <div class="annoncment" style="width: 100%; height: 200px;">
+            <img
+              src="../../assets/imgs/Why-You-Should-Be-Focusing-Your-Marketing-Efforts-On-Mobile-Advertising-And-Social-Media.jpg"
+              alt
+            />
+          </div> -->
+        </div>
+      </div>
+
+
+
+
+
+      <div v-show="tabIndex == 3" class="subjects">
+        <div class="form-title">
+          <h4>
+            <img src="../../assets/imgs/noun_classroom_-1.png" alt />
+            المواد الدراسية
+          </h4>
+        </div>
+        <div class="row">
+          <div
+            class="col-md-3"
+            v-for="(subject, index) in subjects"
+            :key="index"
+          >
+            <nuxt-link :to="'/subjects/teachers'">
+              <div class="subject-cont">
+                <div class="subject-icon-img">
+                  <img :src="subject.icon" alt />
+                </div>
+                <h3>{{ subject.nameAr }}</h3>
+              </div>
+            </nuxt-link>
+          </div>
+
+          <!-- <div class="annoncment" style="width: 100%; height: 200px;">
+            <img
+              src="../../assets/imgs/Why-You-Should-Be-Focusing-Your-Marketing-Efforts-On-Mobile-Advertising-And-Social-Media.jpg"
+              alt
+            />
+          </div> -->
+        </div>
+      </div>
+
+
+       <div v-show="tabIndex == 4" class="subjects">
+        <div class="form-title">
+          <h4>
+            <img src="../../assets/imgs/noun_classroom_-1.png" alt />
+            المواد الدراسية
+          </h4>
+        </div>
+        <div class="row">
+          <div
+            class="col-md-3"
+            v-for="(subject, index) in subjects"
+            :key="index"
+          >
+            <nuxt-link :to="'/best-students'">
+              <div class="subject-cont">
+                <div class="subject-icon-img">
+                  <img :src="subject.icon" alt />
+                </div>
+                <h3>{{ subject.nameAr }}</h3>
+              </div>
+            </nuxt-link>
+          </div>
+
+          <!-- <div class="annoncment" style="width: 100%; height: 200px;">
+            <img
+              src="../../assets/imgs/Why-You-Should-Be-Focusing-Your-Marketing-Efforts-On-Mobile-Advertising-And-Social-Media.jpg"
+              alt
+            />
+          </div> -->
+        </div>
+      </div>
+
+
     </div>
   </div>
 </template>
@@ -107,6 +206,7 @@ export default {
   data() {
     return {
       subjects: [],
+      tabIndex: 1
     }
   },
   created() {
@@ -132,4 +232,21 @@ export default {
 
 <style lang="scss">
 @import '../../assets/sass/subject-dynamic.scss';
+.subjects{
+  margin-top: 20px !important;
+}
+.subjects-navbar{
+  margin-top: 30px;
+  button{
+    font-family: "CustomFontMedium";
+    background: none;
+    border: none;
+    margin-top: 20px;
+    box-shadow: 0px 1px 21px 0px #ddd;
+    img{
+      display: block;
+      margin:auto
+    }
+  }
+}
 </style>
