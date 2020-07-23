9e4436ebe45ac595072fd2c1535bd28db4ab6158
Zeyad Moamen <zeyad.moamen@hamza.solutions>
2020-07-23 04:09:04 +0200

    teachers

---
+++ pages/subjects/_id/teachers/_id/index.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="teacher-info">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h6 class="teacher-info-div">معلومات المدرس</h6>
+
+          <div class="teacher-section">
+            <div class="icon-teacher">
+              <img
+                :src="teacher.photo"
+              />
+            </div>
+            <div class="content-teacher">
+                <h5> {{teacher.username}} </h5>
+            <span>لغة عربية</span>
+            </div>
+
+           
+          </div>
+           <h6 class="teacher-info-div">
+                <nuxt-link :to="'/subjects/1/lectures'" style="font-family:'CustomFontBold';color: #676767;">
+                    المحاضرات
+                </nuxt-link>
+            </h6>
+        </div>
+        
+        <div class="col-md-6">
+          <div class="teacher-buttons">
+            <nuxt-link :to="'/live-details'">
+                <button class="first-btn">
+                    <!-- <img src="../../assets/imgs/live-red.png" alt="" />  --> تفاصيل البث
+                </button>
+            </nuxt-link>
+            <nuxt-link :to="'/timeline'">
+                   <button class="second-btn">
+              <i class="fas fa-stream"></i>
+              التايم لاين
+            </button>
+            </nuxt-link>
+         
+           <nuxt-link :to="'/general-exam-level'">
+                <button class="last-btn">الإمتحانات العامة</button>
+           </nuxt-link>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      teacher: {}
+    }
+  },
+  created(){
+    this.$axios.get(`teachers`).then(res => {
+      this.teacher = res.data.find(teach => teach.id == this.$route.params.id)
+      console.log(res.data)
+      console.log(this.$route.params.id)
+      console.log(this.teacher)
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+.teacher-info {
+  margin-top: 50px;
+  .teacher-info-div {
+    padding: 10px 15px;
+    background-color: #eee;
+    display: inline-block;
+    color: #676767;
+  }
+  .teacher-section {
+    overflow: hidden;
+    padding: 15px 0;
+    .icon-teacher {
+      float: right;
+      width: 100px;
+      border-radius: 50%;
+      overflow: hidden;
+      padding: 2px;
+      border: 1px solid #ccc;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+    .content-teacher{
+        float: right;
+        width: calc(100% - 100px);
+        padding-top: 33px;
+        padding-right: 15px;
+        h5{
+            font-family: "CustomFontBold";
+            margin-bottom: 0;
+        }
+        span{
+            font-family: "CustomFontMedium";
+            color:#8c8c8c;
+        }
+    }
+  }
+  .teacher-buttons {
+    button {
+      display: block;
+      width: 100%;
+      margin-bottom: 5px;
+      text-align: center;
+      padding: 15px 10px;
+      font-size: 19px;
+      border: 0;
+      color: #fff;
+      font-family: 'CustomFontBold';
+      outline: none;
+      
+    }
+    .first-btn {
+        background: #f20333;
+      }
+      .second-btn {
+        background: #068ccb;
+      }
+      .last-btn {
+        background: #068ccb;
+      }
+  }
+}
+</style>
---
+++ pages/subjects/_id/teachers/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="live-details">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="live-details-item">
+              <div class="title">
+                <h4>
+                  <img src="../../../../assets/imgs/live-blue.png" alt="" />
+                  مدرسين المادة
+                </h4>
+              </div>
+            </div>
+          </div>
+
+          <h6 style="margin-top: 15px; font-size: 21px; color: #46a8d5;"></h6>
+        </div>
+
+        <div class="time-content">
+          <div class="row">
+            <div class="col-md-6" v-for="teacher of teachers" :key="teacher.id">
+              <nuxt-link :to="`/subjects/${$route.params.id}/teachers/${teacher.id}`">
+                <div class="time-course" style="position: relative;">
+                  
+                  <div>
+                   <nuxt-link :to="'/general-exam-join'">
+                      <button style="position: absolute;bottom: 0px;left: 0px;background: #088cc0;border: none;color: #FFF;font-family: 'CustomFontBold';">
+                      <div style="position: relative;padding: 6px 19px 0 3px;">
+                        أشتراك الأن
+                        <span
+                          style="
+                                position: absolute;
+                                background-color: rgb(255, 255, 255);
+                                width: 12px;
+                                height: 12px;
+                                top: 8px;
+                                right: 0;
+                                border-radius: 50%;
+                          "
+                        ></span>
+                      </div>
+                    </button>
+                   </nuxt-link>
+                  </div>
+
+                  <div>
+                    <h6> {{teacher.username}} </h6>
+                    <h6 style="color: #6c6c6c;"> {{teacher.email}} </h6>
+                  </div>
+                  <div>
+                    <div class="teacher">
+                      <img :src="teacher.photo" alt="" />
+                      <img src="../../../../assets/imgs/live-red.png" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- this is comment -->
+      <!-- this is comment -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      teachers: [],
+    }
+  },
+  created() {
+    this.$axios
+      .get(`subjects/${this.$route.params.id}/teachers`)
+      .then((res) => {
+        console.log("Teachers",res)
+        this.teachers = res.data
+      })
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../../../assets/sass/saved-lives.scss';
+.live-details .time-content .time-course > div {
+  width: 39.333%;
+}
+.live-details .time-content .time-course > div:last-of-type {
+  width: 19%;
+}
+</style>
